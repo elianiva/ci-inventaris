590a3deda41f7ae7ecaed212a08c212c163a24ca
elianiva <dicha.arkana03@gmail.com>
2021-04-16 14:06:03 +0700

    feat: add Faker for data seeding

---
+++ app/Database/Seeds/Barang.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Barang extends Seeder
+{
+  public function run()
+  {
+    $faker = \Faker\Factory::create("id_ID");
+    $faker->seed(1234);
+
+    for ($i = 0; $i < 50; $i++) {
+      $data = [
+        "kode_barang" => $faker->ean8(),
+        "nama_barang" => ucfirst($faker->word()),
+        "spesifikasi" => $faker->sentence(4),
+        "lokasi_barang" => $faker->streetAddress(),
+        "kategori" => $faker->randomElement([
+          "Pendidikan",
+          "Teknologi",
+          "Kesenian",
+          "Lainnya",
+        ]),
+        "jumlah_barang" => $faker->numberBetween(0, 100),
+        "kondisi" => $faker->randomElement([
+          "Baik",
+          "Kurang Baik",
+          "Tidak Baik",
+        ]),
+        "jenis_barang" => $faker->randomElement([
+          "Alat Tulis Kantor",
+          "Lainnya",
+        ]),
+        "sumber_dana" => $faker->randomElement([
+          "Dana Sekolah",
+          "Dana Bansos",
+          "Lainnya",
+        ]),
+      ];
+
+      $this->db->table("barang")->insert($data);
+    }
+  }
+}
---
+++ app/Database/Seeds/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class User extends Seeder
+{
+  public function run()
+  {
+    $faker = \Faker\Factory::create("id_ID");
+    $faker->seed(1234);
+
+    $gen_pass = fn(string $pass) => password_hash($pass, PASSWORD_BCRYPT);
+
+    $data = [
+      [
+        "nama" => $faker->name(),
+        "username" => "admin",
+        "password" => $gen_pass("admin"),
+        "level" => 1,
+      ],
+      [
+        "nama" => $faker->name(),
+        "username" => "user",
+        "password" => $gen_pass("user"),
+        "level" => 2,
+      ],
+      [
+        "nama" => $faker->name(),
+        "username" => "user_asdf",
+        "password" => $gen_pass("user_asdf"),
+        "level" => 2,
+      ],
+    ];
+    $this->db->table("user")->insertBatch($data);
+  }
+}
