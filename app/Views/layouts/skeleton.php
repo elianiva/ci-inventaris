5dfd7767d26e90c8abbae56e2723b56d5e9363ba
elianiva <dicha.arkana03@gmail.com>
2021-04-18 10:05:15 +0700

    feat: dynamic active sidebar state

--- app/Views/layouts/skeleton.php
+++ app/Views/layouts/skeleton.php
@@ -44,12 +44,12 @@
           <ul class="mt-6">
             <li class="relative px-6 py-3">
               <span
-                class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg <?= $page_name == 'dashboard' ? 'visible' : 'hidden' ?>"
                 aria-hidden="true"
               ></span>
               <a
                 class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                href="index.html"
+                href="<?= base_url("/dashboard") ?>"
               >
                 <svg
                   class="w-5 h-5"
@@ -65,15 +65,19 @@
                     d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                   ></path>
                 </svg>
-                <span class="ml-4"><?= $heading ?></span>
+                <span class="ml-4">Dashboard</span>
               </a>
             </li>
           </ul>
           <ul>
             <li class="relative px-6 py-3">
+              <span
+                class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg <?= $page_name == 'supplier' ? 'visible' : 'hidden' ?>"
+                aria-hidden="true"
+              ></span>
               <a
                 class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="forms.html"
+                href="<?= base_url("/supplier") ?>"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
